26218be6c3afdd5b310e7283e92a2317a9d1985a
FBS0\a.mihnovec <a.mihnovec@fbs.com>
2022-03-29 11:19:09 +0300

    4/3/14 done: prepare for review

--- test_product_page.py
+++ test_product_page.py
@@ -9,25 +9,16 @@ from .pages.basket_page import BasketPage
 import time
 
 
-@pytest.mark.skip
-@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+@pytest.mark.need_review
 def test_quest_can_add_product_to_basket(browser, link):
-    link_url = f"{link}"
+    link_url = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page = ProductPage(browser, link_url)
     page.open()
     page.add_to_basket()
     page.solve_quiz_and_get_code()
     page.assert_item_price()
     page.assert_item_name()
+
 
 @pytest.mark.skip
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
@@ -38,12 +29,14 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     #page.solve_quiz_and_get_code()
     page.should_not_be_success_message()
 
+
 @pytest.mark.skip
 def test_guest_cant_see_success_message(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page = ProductPage(browser, link)
     page.open()
     page.should_not_be_success_message()
+
 
 @pytest.mark.skip
 def test_message_disappeared_after_adding_product_to_basket(browser):
@@ -55,6 +48,7 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     time.sleep(1)
     page.should_disappeared()
 
+
 @pytest.mark.skip
 def test_guest_should_see_login_link_on_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
@@ -62,7 +56,8 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.open()
     page.should_be_login_link()
 
-@pytest.mark.skip
+
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = ProductPage(browser, link)
@@ -73,7 +68,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     time.sleep(10)
 
 
-@pytest.mark.skip
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page = ProductPage(browser, link)
@@ -97,7 +92,7 @@ def test_guest_cant_see_empty_message_in_basket(browser):
     basket_page.should_be_empty_basket()
 
 
-class TestUserAddToBasketFromProductPage():
+class TestUserAddToBasketFromProductPage:
 
     @pytest.fixture(scope="function", autouse="true")
     def setup(self, browser):
@@ -113,6 +108,7 @@ class TestUserAddToBasketFromProductPage():
         page.open()
         page.should_not_be_success_message()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link_url = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
         page = ProductPage(browser, link_url)
